a5c174dd26cfa59abd3e63d5e6a8026f80b8d414
yufenlai0825 <yufenlai0825@gmail.com>
2025-06-13 20:47:31 +0200

    using meta tags to receive data for CSP settings

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -8,8 +8,13 @@
 //     });
 // });
 
-const stripe = Stripe(window.stripePublicKey); // use window-scoped value
+// Read data from HTML <meta> tags
+const stripePublicKey = document.querySelector('meta[name="stripe-public-key"]').content;
+const sessionId = document.querySelector('meta[name="stripe-session-id"]').content;
+
+const stripe = Stripe(stripePublicKey); 
 const orderBtn = document.getElementById("order-btn");
+
 orderBtn.addEventListener("click", function () {
-  stripe.redirectToCheckout({ sessionId: window.sessionId });
+  stripe.redirectToCheckout({ sessionId });
 });
